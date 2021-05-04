115bdbcf6824c06ac965ea7a55f87335d31629a7
Ryma1738 <rymaster.cs@gmail.com>
2021-05-03 23:04:18 -0600

    Learned basic javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto"
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    window.alert("Enemy " + enemyName + "'s health now at: " + enemyHealth);
+    playerHealth= playerHealth - enemyAttack;
+    window.alert(playerName +"'s health now at: " + playerHealth);
+    
+}
+
+fight();
