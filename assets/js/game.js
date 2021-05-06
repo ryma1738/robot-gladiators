f4f13335fc9123b2ab41f494cabe882383922747
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-05 21:28:15 -0600

    Bug Fixing and testing added end functiality and tweaked values to make it harder

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,32 +4,33 @@ var playerHealth = 100;
 var playerBaseHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-var playerHealthTemp = 0;
 
 console.log(playerName, playerHealth, playerAttack);
 
-var enemysName = ["Roborto", "Galotron", "Exo-bot", "X-69420", "Ultron - The Destroyer"];
+var enemysName = ["Roborto", "Galotron", "Exo-bot", "X-69420", "Ultron The Destroyer"];
 var enemysHealth = [50, 75, 90, 110, 150];
-var enemysAttack = [12, 14, 17, 20, 25];
+var enemysAttack = [12, 14, 17, 20, 30];
+var enemysReward = [10, 20, 30, 40, 50];
 var indexEnemy = 0;
 var enemyName = enemysName[indexEnemy];
 var enemyHealth = enemysHealth[indexEnemy];
 var enemyAttack = enemysAttack[indexEnemy];
 var newRound = true;
 var heal = true;
-var hasFought = false;
+var hasFought = 0;
+var end_Game = false;
 
 function fight() {
     if (indexEnemy === 5) {
         window.alert("Congragulations you defeated all the enemy robots! YOU WIN!");
-        break;
+        return end_Game = true;
     }
     else if (newRound) {
         window.alert("Enemy " + enemyName + " has entered the battle! They have " + enemyHealth + " total health and " + enemyAttack + " attack.");
         newRound = false;
+        window.alert("Your health is: " + playerHealth + " and your attack is: " + playerAttack + " and you have: " + playerMoney + " dollars.");
     }
     else {
-        window.alert("Your health is: " + playerHealth + " and you have: " + playerMoney + " dollars.");
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this round? Enter 'FIGHT' or 'SKIP' to chose.");
         promptFight = promptFight.toUpperCase().trim();
         if (promptFight === "FIGHT") {
@@ -38,36 +39,36 @@ function fight() {
 
             if (enemyHealth <= 0) {
                 window.alert(playerName + " killed " + enemyName + "!");
-                window.alert(playerName + " Won and gained 10 dollars!");
+                window.alert(playerName + " Won and gained " + enemysReward[indexEnemy] + " dollars!");
+                playerMoney = playerMoney + enemysReward[indexEnemy];
                 indexEnemy = indexEnemy + 1;
                 enemyName = enemysName[indexEnemy];
                 enemyAttack = enemysAttack[indexEnemy];
                 enemyHealth = enemysHealth[indexEnemy];
-                playerMoney = playerMoney + 10;
                 newRound = true;
             }
 
             else {
-                window.alert(playerName + " attacked " + enemyName +". " + enemyName + " now has " + enemyHealth + " health left. You gained 1 dollar");
-                playerMoney = playerMoney + 1;
+                window.alert(playerName + " attacked " + enemyName +". " + enemyName + " now has " + enemyHealth + " health left. You gained $" + (enemysReward[indexEnemy] / 10));
+                playerMoney = playerMoney + (enemysReward[indexEnemy] / 10);
                 playerHealth = playerHealth - enemyAttack;
 
                 if (playerHealth <= 0) {
                     window.alert(enemyName + " killed " + playerName + "!");
-                    window.alert("Game Over: Refresh the page to play again!");
-                    break;
+                    window.alert("Game Over");
+                    return end_Game = true;
                 }
         
                 else {
                     window.alert(enemyName + " attacked " + playerName +". " + playerName + " now has " + playerHealth + " health left.");
                 }
             }
-            hasFought = true;
+            hasFought = hasFought + 1;
 
         }
 
         else if (promptFight == "SKIP") {
-            var skip = window.confirm("If you skip this round  you will lose 2 dollars. Do you still want to skip? YES or NO?");
+            var skip = window.confirm("If you skip this round  you will lose 2 dollars. You currently have $" + playerMoney + ". Do you still want to skip? YES or NO?");
             if (skip) {
                 window.alert("You have skiped the round and lost 2 dollars");
                 playerMoney = playerMoney - 2;
@@ -87,28 +88,28 @@ function fight() {
 
 function store_or_heal() {
     while (true) {
-        var store = window.prompt("Would you like to heal your self by 15% for free or buy items at the shop? Enter 'HEAL' or 'SHOP' or 'EXIT' (To return to battling).");
+        var store = window.prompt("Would you like to heal your self by 25% for free or buy items at the shop? Enter 'HEAL' or 'SHOP' or 'EXIT' (To return to battling).");
         store = store.toUpperCase().trim();
         if (store === "HEAL") {
-            if (hasFought) {
-                playerHealthTemp = playerHealth + (playerBaseHealth * .15);
-                hasFought = false;
+            if (hasFought >= 2) {
+                var playerHealthTemp = playerHealth + (playerBaseHealth * .25);
+                hasFought = 0;
                 if (playerHealthTemp > playerBaseHealth) {
                     playerHealth = playerBaseHealth;
                 }
                 else {
-                    playerhealth = playerHealthTemp;
+                    playerHealth = playerHealthTemp;
                 }
                 window.alert(playerName + "'s health is now " + playerHealth + "!");
             }
             else {
-                window.alert("You can only heal after fighting at least 1 time, and you can not heal back to back.");
+                window.alert("You can only heal after fighting at least 2 times, and you can not heal back to back.");
             }
         }
 
-        else if (store === "STORE") {
+        else if (store === "SHOP") {
             while (true) {
-                window.alert("Your currently have $" + playerMoney + ".");
+                window.alert("You currently have $" + playerMoney + ".");
                 var item = window.prompt("What would like to buy? Increase Max Health (IMH) $20, Increase Attack Damage (AD) $15, or Heal 20 Damage (H) $10? Please enter either IMH, AD, H or E to exit the store.");
                 item = item.toUpperCase().trim();
 
@@ -149,7 +150,7 @@ function store_or_heal() {
                         if ((playerMoney - 10) < 0) {
                             window.alert("You Dont have enough money to buy this.");
                         }
-                        else if (playerHealth = playerBaseHealth) {
+                        else if (playerHealth === playerBaseHealth) {
                             window.alert("Your Health is already at its max! Please purchase more health to heal more.");
                         }
                         else {
@@ -158,7 +159,7 @@ function store_or_heal() {
                                 playerHealth = playerBaseHealth;
                             }
                             else {
-                                playerhealth = playerhealth + 20;
+                                playerHealth = playerHealth + 20;
                             }
                             window.alert("Your health is now " + playerHealth + " and you now have " + playerMoney + " dollars left.");
                         }
@@ -183,12 +184,6 @@ function store_or_heal() {
             store_or_heal();
         }
     }
-    //give players the option to heal or go to store
-    //    *if they chose to heal check to see if they healed last time
-    //     *If they did heal last time push them back to fight with a error message
-    //Give players multiple options for things they can buy and display current health, attack, and money
-    //     * Have it be a text field options for catagories with a while loop
-    //      * One of the options should be a leave store option which pushes them back to the fight
 }
 
 function confirmPurchase(dollars, option) {
@@ -196,6 +191,18 @@ function confirmPurchase(dollars, option) {
     return purchase;
 }
 
-while (true) {
-    fight();
+function endGame() {
+    end_Game = true;
+    return end_game;
 }
+
+while (true) {
+    if (end_Game) {
+        break;
+    }
+    else {
+        fight();
+    }
+    
+}
+window.alert("Thank you for playing! Refresh the page to play again.");
